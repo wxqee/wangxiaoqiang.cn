b80a18b0d1e795d8813487c5857163e8ea41f49c
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2019-12-20 20:24:38 +0800

    update title, remove favicon, add 备案号

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,11 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <footer>
+        备案号: <a href={'https://www.miitbeian.gov.cn'}>苏ICP备15011962号</a>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
